6cedde91076a120ea917f5f7fd78895381ca788e
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-10-20 15:47:34 -0400

    hover effects portfolio and flavicon

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const projectDescription = document.querySelector('#portfolio-description'); // container of each project description
 const about = document.querySelector('#portfolio-about');
+const main = document.querySelector('main');
 
-const hoverProject = (description) => {
+const hoverProject = (description, image) => {
     if (description !== "")
         about.style.visibility = "visible";
     else
         about.style.visibility = "hidden";
 
+    if (image) {
+        main.style.backgroundImage = image;
+        main.style.backgroundSize = "cover";
+    }
+    else 
+        main.style.backgroundImage = "none";
+        
+    projectDescription.style.display = "none";
     projectDescription.innerHTML = description;
+    projectDescription.style.display = "block";
 };
 
 
